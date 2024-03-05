9a539e5bf931713bd6f78e6ed5a85337eb69a8b7
hammadnadir <hammadnadir75@gmail.com>
2024-03-06 02:49:38 +0500

    ooo

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,52 +1,59 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const nextAuthOptions = {
-	providers: [
-		CredentialsProvider({
-			name: 'credentials',
-			credentials: {
-				email: { label: 'email', type: 'text' },
-				password: { label: 'password', type: 'password' }
-			},
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
 
-			async authorize(credentials, req) {
-				const response = await fetch('https://car-app-backend.vercel.app/user/signin', {
-					method: 'POST',
-					headers: {
-						'Content-type': 'application/json'
-					},
-					body: JSON.stringify({
-						email: credentials?.email,
-						password: credentials?.password
-					})
-				})
+      async authorize(credentials, req) {
+        const response = await fetch(
+          "https://car-app-backend.vercel.app/user/signin",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          }
+        );
 
-				const user = await response.json()
+        const user = await response.json();
 
-				if (user) {
-					return user
-				}
+        if (user) {
+          return user;
+        }
 
-				return null
-			},
-		})
-	],
-	pages: {
-		signIn: '/'
-	},
-	callbacks: {
-		async jwt({ token, user }) {
-			user && (token.user = user)
-			return token
-		},
-		async session({ session, token }){
-			session = token.user
-			return session
-		}
-	}
-}
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  session: {
+    jwt: true,
+  },
+  secret: process.env.SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      user && (token.user = user);
+      return token;
+    },
+    async session({ session, token }) {
+      session = token.user;
+      return session;
+    },
+  },
+};
 
-const handler = NextAuth(nextAuthOptions)
+const handler = NextAuth(nextAuthOptions);
 
-export { handler as GET, handler as POST, nextAuthOptions }
+export { handler as GET, handler as POST, nextAuthOptions };
